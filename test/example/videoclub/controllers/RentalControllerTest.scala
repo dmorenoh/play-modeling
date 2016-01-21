548e1ece114cb318991ee82e5d8c4f7c8b5024f5
Dragisa Krsmanovic <dragishak@gmail.com>
2016-01-21 13:57:47 -0800

    Add Play server test

--- test/example/videoclub/controllers/RentalControllerTest.scala
+++ test/example/videoclub/controllers/RentalControllerTest.scala
@@ -1,11 +1,12 @@
 package example.videoclub.controllers
 
+import play.api.libs.ws.WS
 import play.api.mvc.Results
 import play.api.test._
 import play.api.test.Helpers._
 import org.scalatestplus.play._
 
-class RentalControllerTest extends PlaySpec with Results {
+class RentalControllerTest extends PlaySpec with Results with OneServerPerSuite {
 
   "helloWorld" must {
     "return Hello World" in new App {
@@ -18,4 +19,18 @@ class RentalControllerTest extends PlaySpec with Results {
       contentAsString(result) mustEqual "Hello World"
     }
   }
+
+  "server" must {
+    "return Hello World" in {
+
+      val response = await(WS.url(s"http://localhost:$port/helloWorld").get())
+
+      response.status mustBe OK
+
+      response.body mustEqual "Hello World"
+    }
+
+  }
+
+
 }
