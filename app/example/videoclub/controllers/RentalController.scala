0d35e859ace2cc103529badf15d3b5e063ebfcaf
Dragisa Krsmanovic <dragishak@gmail.com>
2016-01-29 13:59:30 -0800

    Start writing controller

--- app/example/videoclub/controllers/RentalController.scala
+++ app/example/videoclub/controllers/RentalController.scala
@@ -1,16 +1,33 @@
 package example.videoclub.controllers
 
-import javax.inject.Singleton
-
+import example.videoclub.repository._
+import example.videoclub.services._
+import play.api.libs.json._
 import play.api.mvc._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import scalaz._
+import Scalaz._
 
-trait RentalController extends Controller {
+abstract class RentalController[Movie: Writes, DVD: Writes, Customer, Timestamp, Repo <: Repository] extends Controller {
+
+  type Result[A] = ServiceResult[A, Repo]
+
+  def service: RentalService[Result, Movie, Customer, DVD, Timestamp]
+
+  def repository: Repo
 
   def helloWorld() = Action {
     Ok("Hello World")
   }
 
+  def addDVDs(movie: Movie, qty: Int) = run(service.addMovie(movie, qty))
+
+  private def run[A: Writes](result: Result[A]) = Action.async {
+    result.run(repository).fold(
+      error => BadRequest(error),
+      res => Ok(Json.toJson(res))
+    )
+  }
+
 }
 
-@Singleton
-class RentalControllerImpl extends RentalController
